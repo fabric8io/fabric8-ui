2d07c64b84a68c3b8f024b4971610a54aa1c814b
Konrad Kleine <kwk@users.noreply.github.com>
2017-05-24 18:12:36 +0200

    fix sourcing of jenkins environment file
    This change fixes this error:
    
    ```
    + source jenkins-env
    ++ ghprbPullId=1368
    ++ BUILD_URL=https://ci.centos.org/job/devtools-fabric8-ui/1486/
    ++ ghprbPullLink=https://github.com/fabric8io/fabric8-ui/pull/1368
    ++ HUDSON_SERVER_COOKIE=f4d3a7a925dabf8a
    ++ SHELL=/bin/bash
    ++ ghprbSourceBranch=codecov.setup
    ++ ghprbPullAuthorEmail=corinnekrych@gmail.com
    ++ BUILD_TAG=jenkins-devtools-fabric8-ui-1486
    ++ ghprbPullAuthorLogin=corinnekrych
    ++ GIT_PREVIOUS_COMMIT=b3621747d8933e8d2b54d17af3c29b7ba9c66acd
    ++ ROOT_BUILD_CAUSE=GHPRBCAUSE
    ++ WORKSPACE=/home/devtools_poc/workspace/devtools-fabric8-ui
    ++ JOB_URL=https://ci.centos.org/job/devtools-fabric8-ui/
    ++ ghprbPullDescription=GitHub
    ++ pull request
    jenkins-env: line 14: pull: command not found
    ```
    
    See also: https://ci.centos.org/job/devtools-fabric8-ui/1486/console

--- upload_to_codecov.sh
+++ upload_to_codecov.sh
@@ -8,7 +8,9 @@ set -e
 
 # Ensure all Jenkins variables are set (e.g. commit, branch, etc.)
 if [ -e "jenkins-env" ]; then
-  source jenkins-env
+  # Only try to source lines with an equal sign in them.
+  grep "=" jenkins-env > jenkins-env.clean
+  source jenkins-env.clean
 fi
 
 bash <(curl -s https://codecov.io/bash) -t 274c63b8-b698-425d-a0ab-6a4020eca599 -f coverage/coverage.json
