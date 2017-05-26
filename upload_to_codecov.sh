aabf9605f5cffb0e5838350d7103665d91e07c7f
Konrad Kleine <kwk@users.noreply.github.com>
2017-05-26 16:10:14 +0200

    fix(env): filter of env vars
    I forgot to add the cat.

--- upload_to_codecov.sh
+++ upload_to_codecov.sh
@@ -8,7 +8,8 @@ set -e
 
 # Ensure all Jenkins variables are set (e.g. commit, branch, etc.)
 if [ -e "jenkins-env" ]; then
-  grep -E "(JENKINS_URL|GIT_BRANCH|GIT_COMMIT|BUILD_NUMBER|ghprbSourceBranch|ghprbActualCommit|BUILD_URL|ghprbPullId)=" \
+  cat jenkins-env \
+  | grep -E "(JENKINS_URL|GIT_BRANCH|GIT_COMMIT|BUILD_NUMBER|ghprbSourceBranch|ghprbActualCommit|BUILD_URL|ghprbPullId)=" \
   | sed 's/^/export /g' \		
     > ~/.jenkins-env
   source ~/.jenkins-env
