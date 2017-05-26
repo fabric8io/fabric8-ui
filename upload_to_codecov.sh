78ee19bcad6055732491bbc30a51b672f6926abf
Konrad Kleine <kwk@users.noreply.github.com>
2017-05-26 08:12:00 +0200

    fix sourcing of env vars
    Once https://github.com/almighty/almighty-jobs/pull/184 is approved and merged, this should pass tests.

--- upload_to_codecov.sh
+++ upload_to_codecov.sh
@@ -8,11 +8,7 @@ set -e
 
 # Ensure all Jenkins variables are set (e.g. commit, branch, etc.)
 if [ -e "jenkins-env" ]; then
-  # Only try to source lines with an equal sign in them.
-  grep "=" jenkins-env > jenkins-env.clean
-  if [ -e "jenkins-env.clean" ]; then
-    source jenkins-env.clean
-  fi
+  source jenkins-env
 fi
 
 bash <(curl -s https://codecov.io/bash) -t 274c63b8-b698-425d-a0ab-6a4020eca599 -f coverage/coverage.json
