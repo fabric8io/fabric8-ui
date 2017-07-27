6190eee4a6b8743365a480e8d3e0bafeb868169f
joshuawilson <javajoshw@gmail.com>
2017-07-27 03:37:47 -0400

    fix(toggle): add bubbles to the flask

--- src/app/exp-feature-page/exp-feature-page.component.ts
+++ src/app/exp-feature-page/exp-feature-page.component.ts
@@ -5,14 +5,15 @@ import {
   EventEmitter,
   Output
 } from '@angular/core';
-
+import { TweenMax, TimelineMax, Power2 } from 'gsap';
 import 'rxjs/add/operator/map';
 import { Notifications, Notification, NotificationType } from 'ngx-base';
 import { GettingStartedService } from '../getting-started/services/getting-started.service';
 
 @Component({
   selector: 'f8-exp-feature-page',
-  templateUrl: './exp-feature-page.component.html'
+  templateUrl: './exp-feature-page.component.html',
+  styleUrls: ['./exp-feature-page.component.less']
 })
 export class ExpFeaturePageComponent implements OnInit, OnDestroy {
 
@@ -26,6 +27,20 @@ export class ExpFeaturePageComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.enableFeatures = false;
+
+    TweenMax.set('svg', {
+      visibility: 'visible'
+    });
+
+    let targetObject = document.getElementsByTagName('circle');
+    let tl = new TimelineMax();
+    tl.staggerTo(targetObject, 3, {
+      attr: {
+        cy: 200
+      },
+      ease:Power2.easeIn,
+      repeat: -1
+    }, 0.6)
   }
 
   ngOnDestroy() {
