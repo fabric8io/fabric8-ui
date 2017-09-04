fe42a1eb86649de0ad8b89d8d03bd03e551c2ac7
Corinne Krych <corinnekrych@gmail.com>
2017-09-04 09:03:27 +0200

    Make create-space separate from wizard

--- src/app/space/wizard/components/space-creator/space-creator.component.ts
+++ src/app/space/wizard/components/space-creator/space-creator.component.ts
@@ -1,5 +1,5 @@
-import { SpacesService } from '../../../../shared/spaces.service';
-import { Component, Input, OnInit } from '@angular/core';
+import { IModalHost } from '../../models/modal-host';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Notification, NotificationAction, Notifications, NotificationType } from 'ngx-base';
@@ -9,26 +9,32 @@ import {
 } from 'ngx-fabric8-wit';
 import { UserService } from 'ngx-login-client';
 import { Observable } from 'rxjs';
-import { DummyService } from '../../../../shared/dummy.service';
-import { SpaceNamespaceService } from '../../../../shared/runtime-console/space-namespace.service';
 import { ProcessTemplate } from 'ngx-fabric8-wit';
 import { ILoggerDelegate, LoggerFactory } from '../../common/logger';
 import { IWorkflow } from '../../models/workflow';
-import { AppGeneratorConfiguratorService } from '../../services/app-generator.service';
+import { DummyService } from "app/shared/dummy.service";
+import { SpaceNamespaceService } from "app/shared/runtime-console/space-namespace.service";
+import { SpacesService } from "app/shared/spaces.service";
+import { AppGeneratorConfiguratorService } from "app/space/wizard/services/app-generator.service";
+import {
+  Space,
+  SpaceAttributes
+} from 'ngx-fabric8-wit';
 
 @Component({
   selector: 'space-creator',
   templateUrl: './space-creator.component.html',
-  styleUrls: ['./space-creator.component.less'],
-  providers: [SpaceService]
+  styleUrls: ['./space-creator.component.less']
 })
-export class SpaceCreatorComponent implements OnInit {
+export class SpaceCreatorComponent implements OnInit, OnDestroy {
 
   static instanceCount: number = 1;
 
-  @Input() workflow: IWorkflow = null;
+  @Input() host: IModalHost;
+
   spaceTemplates: ProcessTemplate[];
   selectedTemplate: ProcessTemplate;
+  space: Space;
 
   constructor(
     private router: Router,
@@ -38,40 +44,30 @@ export class SpaceCreatorComponent implements OnInit {
     private userService: UserService,
     private spaceNamespaceService: SpaceNamespaceService,
     private spaceNamePipe: SpaceNamePipe,
-    public configurator: AppGeneratorConfiguratorService,
-    private spacesService: SpacesService,
-    loggerFactory: LoggerFactory
+    private spacesService: SpacesService
   ) {
-    let logger = loggerFactory.createLoggerDelegate(this.constructor.name, SpaceCreatorComponent.instanceCount++);
-    if (logger) {
-      this.log = logger;
-    }
-    this.log(`New instance ...`);
     this.spaceTemplates = dummy.processTemplates;
+    this.space = this.createTransientSpace();
   }
 
-  ngOnInit() {
-
-    this.log(`ngInit ...`);
-    const srumTemplates = this.spaceTemplates.filter(template => template.name == "Scenario Driven Planning")
-    if (srumTemplates && srumTemplates.length > 0) {
-      this.selectedTemplate = srumTemplates[0];
-    }
-  }
+ngOnDestroy() {
+  this.finish()
+}
 
   /*
    * Creates a persistent collaboration space
    * by invoking the spaceService
    */
   createSpace() {
-    this.log(`createSpace ...`);
-    let space = this.configurator.transientSpace;
-    console.log('Creating space', space);
-    space.attributes.name = space.name.replace(/ /g, '_');
+    console.log('Creating space', this.space);
+    if (!this.space) {
+      this.space = this.createTransientSpace();
+    }
+    this.space.attributes.name = this.space.name.replace(/ /g, '_');
     this.userService.getUser()
       .switchMap(user => {
-        space.relationships['owned-by'].data.id = user.id;
-        return this.spaceService.create(space);
+        this.space.relationships['owned-by'].data.id = user.id;
+        return this.spaceService.create(this.space);
       })
       .do(createdSpace => {
         this.spacesService.addRecent.next(createdSpace);
@@ -84,7 +80,7 @@ export class SpaceCreatorComponent implements OnInit {
           .catch(err => Observable.of(createdSpace));
       })
       .subscribe(createdSpace => {
-        this.configurator.currentSpace = createdSpace;
+        //this.configurator.currentSpace = createdSpace;
         const primaryAction: NotificationAction = {
           name: `Open Space`,
           title: `Open ${this.spaceNamePipe.transform(createdSpace.attributes.name)}`,
@@ -99,24 +95,64 @@ export class SpaceCreatorComponent implements OnInit {
         .subscribe(action => {
           this.router.navigate([createdSpace.relationalData.creator.attributes.username,
           createdSpace.attributes.name]);
-          this.workflow.cancel();
+          this.finish();
         });
-        this.workflow.gotoNextStep();
+        this.finish();
       },
       err => {
         console.log('Error creating space', err);
         this.notifications.message(<Notification>{
-          message: `Failed to create "${space.name}"`,
+          message: `Failed to create "${this.space.name}"`,
           type: NotificationType.DANGER
         });
-        this.workflow.cancel();
+        this.finish();
       });
   }
 
-  /**
-   * used to add a log entry to the logger
-   * The default one shown here does nothing.
-   */
-  log: ILoggerDelegate = () => { };
+  ngOnInit() {
+    this.host.closeOnEscape = true;
+    this.host.closeOnOutsideClick = false;
+    const srumTemplates = this.spaceTemplates.filter(template => template.name == "Scenario Driven Planning")
+    if (srumTemplates && srumTemplates.length > 0) {
+      this.selectedTemplate = srumTemplates[0];
+    }
+  }
 
+  finish() {
+    this.space = this.createTransientSpace();
+
+    if (this.host) {
+      this.host.close();
+    }
+  }
+
+  private createTransientSpace(): Space {
+    let space = {} as Space;
+    space.name = '';
+    space.path = '';
+    space.attributes = new SpaceAttributes();
+    space.attributes.name = space.name;
+    space.type = 'spaces';
+    space.privateSpace = false;
+    space.process = { name: '', description: '' };
+    space.relationships = {
+      areas: {
+        links: {
+          related: ''
+        }
+      },
+      iterations: {
+        links: {
+          related: ''
+        }
+      },
+      ['owned-by']: {
+        data: {
+          id: '',
+          type: 'identities'
+        }
+      }
+    };
+    return space;
+  }
 }
