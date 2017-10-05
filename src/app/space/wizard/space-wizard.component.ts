fa7331580cd3d8547f204f6ff68d795944f0e2da
Corinne Krych <corinnekrych@gmail.com>
2017-10-05 13:54:19 +0200

    fix(forge-wizard): SpaceWizard error

--- src/app/space/wizard/space-wizard.component.ts
+++ src/app/space/wizard/space-wizard.component.ts
@@ -1,25 +1,22 @@
-//import { IModalHost } from './models/modal-host';
 import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Notification, NotificationAction, Notifications, NotificationType } from 'ngx-base';
 import {
   SpaceService,
-  SpaceNamePipe
+  SpaceNamePipe, Context
 } from 'ngx-fabric8-wit';
 import { UserService } from 'ngx-login-client';
 import { Observable } from 'rxjs';
 import { ProcessTemplate } from 'ngx-fabric8-wit';
-import { ILoggerDelegate, LoggerFactory } from '../../common/logger';
-import { IWorkflow } from '../../models/workflow';
-import { DummyService } from "app/shared/dummy.service";
-import { SpaceNamespaceService } from "app/shared/runtime-console/space-namespace.service";
-import { SpacesService } from "app/shared/spaces.service";
-//import { AppGeneratorConfiguratorService } from "app/space/wizard/services/app-generator.service";
+import { DummyService } from 'app/shared/dummy.service';
+import { SpaceNamespaceService } from 'app/shared/runtime-console/space-namespace.service';
+import { SpacesService } from 'app/shared/spaces.service';
 import {
   Space,
   SpaceAttributes
 } from 'ngx-fabric8-wit';
+import { ContextService } from 'app/shared/context.service';
 
 @Component({
   selector: 'space-wizard',
@@ -28,13 +25,12 @@ import {
 })
 export class SpaceWizardComponent implements OnInit, OnDestroy {
 
-  static instanceCount: number = 1;
-
   @Input() host: any;
 
   spaceTemplates: ProcessTemplate[];
   selectedTemplate: ProcessTemplate;
   space: Space;
+  currentSpace: Space;
 
   constructor(
     private router: Router,
@@ -44,15 +40,22 @@ export class SpaceWizardComponent implements OnInit, OnDestroy {
     private userService: UserService,
     private spaceNamespaceService: SpaceNamespaceService,
     private spaceNamePipe: SpaceNamePipe,
-    private spacesService: SpacesService
+    private spacesService: SpacesService,
+    private context: ContextService
   ) {
     this.spaceTemplates = dummy.processTemplates;
     this.space = this.createTransientSpace();
+    this.context.current.subscribe((ctx: Context) => {
+      if (ctx.space) {
+        this.currentSpace = ctx.space;
+        console.log(`ForgeWizardComponent::The current space has been updated to ${this.currentSpace.attributes.name}`);
+      }
+    });
   }
 
-ngOnDestroy() {
-  this.finish()
-}
+  ngOnDestroy() {
+    this.finish();
+  }
 
   /*
    * Creates a persistent collaboration space
@@ -80,46 +83,45 @@ ngOnDestroy() {
           .catch(err => Observable.of(createdSpace));
       })
       .subscribe(createdSpace => {
-        //this.configurator.currentSpace = createdSpace;
-        const primaryAction: NotificationAction = {
-          name: `Open Space`,
-          title: `Open ${this.spaceNamePipe.transform(createdSpace.attributes.name)}`,
-          id: 'openSpace'
-        };
-        this.notifications.message(<Notification>{
-          message: `Your new space is created!`,
-          type: NotificationType.SUCCESS,
-          primaryAction: primaryAction
-        })
-        .filter(action => action.id === primaryAction.id)
-        .subscribe(action => {
-          this.router.navigate([createdSpace.relationalData.creator.attributes.username,
-          createdSpace.attributes.name]);
+          const primaryAction: NotificationAction = {
+            name: `Open Space`,
+            title: `Open ${this.spaceNamePipe.transform(createdSpace.attributes.name)}`,
+            id: 'openSpace'
+          };
+          this.notifications.message(<Notification>{
+            message: `Your new space is created!`,
+            type: NotificationType.SUCCESS,
+            primaryAction: primaryAction
+          })
+            .filter(action => action.id === primaryAction.id)
+            .subscribe(action => {
+              this.router.navigate([createdSpace.relationalData.creator.attributes.username,
+                createdSpace.attributes.name]);
+              this.finish();
+            });
+          this.finish();
+        },
+        err => {
+          console.log('Error creating space', err);
+          this.notifications.message(<Notification>{
+            message: `Failed to create "${this.space.name}"`,
+            type: NotificationType.DANGER
+          });
           this.finish();
         });
-        this.finish();
-      },
-      err => {
-        console.log('Error creating space', err);
-        this.notifications.message(<Notification>{
-          message: `Failed to create "${this.space.name}"`,
-          type: NotificationType.DANGER
-        });
-        this.finish();
-      });
   }
 
   ngOnInit() {
     this.host.closeOnEscape = true;
     this.host.closeOnOutsideClick = false;
-    const srumTemplates = this.spaceTemplates.filter(template => template.name == "Scenario Driven Planning")
+    const srumTemplates = this.spaceTemplates.filter(template => template.name === 'Scenario Driven Planning');
     if (srumTemplates && srumTemplates.length > 0) {
       this.selectedTemplate = srumTemplates[0];
     }
   }
 
   finish() {
-    this.log(`finish ...`);
+    console.log(`finish ...`);
     // navigate to the users space if they aren't there already
     if (
       this.router &&
@@ -129,18 +131,17 @@ ngOnDestroy() {
       this.router.routerState.snapshot.root.firstChild &&
       this.router.routerState.snapshot.root.firstChild.params['space']
     ) {
-      this.log('Wizard complete and already in space context, no need to move.');
+      console.log('Wizard complete and already in space context, no need to move.');
     } else {
       this.router.navigate([
-        this.configurator.currentSpace.relationalData.creator.attributes.username,
-        this.configurator.currentSpace.attributes.name
+        this.currentSpace.relationalData.creator.attributes.username,
+        this.currentSpace.attributes.name
       ]);
     }
     if (this.host) {
       this.host.close();
     }
   }
-
 
   private createTransientSpace(): Space {
     let space = {} as Space;
