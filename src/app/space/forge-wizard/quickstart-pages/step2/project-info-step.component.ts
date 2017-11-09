7d8ed23ab4ac1600bbf1db84b085bca40a464980
Corinne Krych <corinnekrych@gmail.com>
2017-11-09 16:12:34 +0100

    fix(forge-wizard): project name validation to match forge backend

--- src/app/space/forge-wizard/quickstart-pages/step2/project-info-step.component.ts
+++ src/app/space/forge-wizard/quickstart-pages/step2/project-info-step.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Gui, Input as GuiInput } from 'ngx-forge';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'project-info-step',
@@ -19,8 +19,14 @@ export class ProjectInfoStepComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
+    // Add validation to fit server side validation
+    this.form.controls['named'].setValidators([Validators.pattern(/^[a-z][a-z0-9\-]*$/),
+      Validators.minLength(2),
+      Validators.maxLength(24)]);
+
     // Default value for the project name to space name
-    this.form.controls['named'].setValue(this.labelSpace.toLowerCase());
+    this.form.controls['named'].setValue(this.getValidDefaultName(this.labelSpace));
+
     if (this.gui.inputs && this.gui.inputs.length > 3) {
       this.organisation = this.gui.inputs[0];
       this.repoName = this.gui.inputs[1];
@@ -31,7 +37,15 @@ export class ProjectInfoStepComponent implements OnInit {
       this.groupId = this.gui.inputs[1];
       this.repoVersion = this.gui.inputs[2];
     }
-    console.log(':::label' + this.labelSpace);
+  }
+
+  private getValidDefaultName(projectName: string): string {
+    let defaultName = projectName.toLowerCase();
+    if (defaultName.length > 24) {
+      defaultName = defaultName.substring(0, 23);
+    }
+    defaultName = defaultName.replace(/_/g, '');
+    return defaultName;
   }
 
 }
