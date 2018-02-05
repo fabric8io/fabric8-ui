e02d59bc3aeafb69139be0bdf732cc9c3b9e7555
Corinne Krych <corinnekrych@gmail.com>
2018-02-05 17:58:36 +0100

    fix(feature-flag): remove unsused import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -59,7 +59,7 @@ import { AppState, InternalStateType } from './app.service';
 // Footer & Header
 import { FeatureBannerComponent } from './feature-flag/banner/feature-banner.component';
 import { FeatureFlagResolver } from './feature-flag/resolver/feature-flag.resolver';
-import { FABRIC8_FEATURE_TOGGLES_API_URL, featureTogglesApiUrlProvider, FeatureTogglesService } from './feature-flag/service/feature-toggles.service';
+import { featureTogglesApiUrlProvider, FeatureTogglesService } from './feature-flag/service/feature-toggles.service';
 import { FeatureWarningPageComponent } from './feature-flag/warning-page/feature-warning-page.component';
 
 import { FooterComponent } from './layout/footer/footer.component';
