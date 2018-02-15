e29b9e03d8b5972dac6c4c63aad61b8dbb927576
SMahil <mahilsukh11@gmail.com>
2018-02-15 12:05:13 +0530

    fix(email-verification): fix template with error message

--- src/app/profile/email-verification/email-verification.component.ts
+++ src/app/profile/email-verification/email-verification.component.ts
@@ -11,7 +11,12 @@ import {
 })
 export class EmailVerificationComponent implements OnInit {
 
-  private verified: boolean = false;
+  private verified: string = 'false';
+  private message: string = 'Your e-mail has been confirmed.';
+  private secMessage: string = 'Thank you for validating your e-mail address.' +
+  'You can now continue to use Openshift.io';
+  private messageEmoji: string = 'trophy.png';
+
   constructor(
     private route: ActivatedRoute) {}
 
@@ -19,7 +24,12 @@ export class EmailVerificationComponent implements OnInit {
     const queryParams = this.route.snapshot.queryParams;
     if (this.route.snapshot.queryParams['verified']) {
       this.verified = this.route.snapshot.queryParams['verified'];
-      console.log('verified', this.verified);
+      if (this.verified === 'false') {
+        this.message = this.route.snapshot.queryParams['error'];
+        this.secMessage = 'Your e-mail has already been approved. ' +
+        'Click on my profile below to continue.';
+        this.messageEmoji = 'neutralface.png';
+      }
     }
   }
 }
