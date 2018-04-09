c0664e606e3480eabfdb0f6ccb2529732acd1e7d
Andrew Azores <me@andrewazor.es>
2018-04-09 15:09:27 -0400

    fix(environment-widget): ApplicationOverviewService polls for updates
    ApplicationOverviewService gains ability to poll (every 10 seconds) for updates to its data,
    restoring poll functionality previously available in DeploymentsService
    
    related to https://github.com/openshiftio/openshift.io/issues/2961

--- src/app/dashboard-widgets/environment-widget/application-overview.service.ts
+++ src/app/dashboard-widgets/environment-widget/application-overview.service.ts
@@ -1,6 +1,12 @@
-import { Injectable } from '@angular/core';
+import {
+  Injectable,
+  OnDestroy
+} from '@angular/core';
 
-import { Observable } from 'rxjs';
+import {
+  Observable,
+  Subject
+} from 'rxjs';
 
 import {
   Application,
@@ -20,26 +26,42 @@ export interface DeploymentPreviewInfo {
 }
 
 @Injectable()
-export class ApplicationOverviewService {
+export class ApplicationOverviewService implements OnDestroy {
+
+  private readonly destroyed: Subject<void> = new Subject<void>();
+
+  private readonly pollTimer: Observable<void> = Observable.timer(0, 10000)
+    .map(() => null)
+    .takeUntil(this.destroyed);
 
   constructor(
     private deploymentApiService: DeploymentApiService
   ) { }
 
+  ngOnDestroy(): void {
+    this.destroyed.next();
+    this.destroyed.complete();
+  }
+
   getAppsAndEnvironments(spaceId: string): Observable<ApplicationAttributesOverview[]> {
-    return this.deploymentApiService.getApplications(spaceId)
-      .map((apps: Application[]): Application[] => apps || [])
-      .map((apps: Application[]): ApplicationAttributesOverview[] =>
-        apps.map((app: Application): ApplicationAttributesOverview => {
-          const appName: string = app.attributes.name;
-          const deploymentsInfo: DeploymentPreviewInfo[] = app.attributes.deployments.map(
-            (dep: Deployment): DeploymentPreviewInfo => ({
-              name: dep.attributes.name, version: dep.attributes.version, url: dep.links.application
-            })
-          );
-          return { appName, deploymentsInfo };
-        })
-      );
+    return this.pollTimer.mergeMap(() =>
+      this.deploymentApiService.getApplications(spaceId)
+        .map((apps: Application[]): Application[] => apps || [])
+        .map((apps: Application[]): Application[] => apps.sort((a: Application, b: Application): number => a.attributes.name.localeCompare(b.attributes.name)))
+        .map((apps: Application[]): ApplicationAttributesOverview[] =>
+          apps.map((app: Application): ApplicationAttributesOverview => {
+            const appName: string = app.attributes.name;
+            const deploymentsInfo: DeploymentPreviewInfo[] = app.attributes.deployments.map(
+              (dep: Deployment): DeploymentPreviewInfo => ({
+                name: dep.attributes.name, version: dep.attributes.version, url: dep.links.application
+              })
+            ).sort((a: DeploymentPreviewInfo, b: DeploymentPreviewInfo): number => a.name.localeCompare(b.name));
+            return { appName, deploymentsInfo };
+          })
+        )
+    )
+      .distinctUntilChanged()
+      .shareReplay();
   }
 
 }
