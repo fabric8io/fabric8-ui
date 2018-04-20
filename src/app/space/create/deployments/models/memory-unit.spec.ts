7db6e766b34aad43f714bfd6c62cdcd581fd92fa
Andrew Azores <me@andrewazor.es>
2018-04-20 15:37:02 -0400

    test(deployments): add tests for MemoryUnit fromOrdinal
    related to https://github.com/openshiftio/openshift.io/issues/2786

--- src/app/space/create/deployments/models/memory-unit.spec.ts
+++ src/app/space/create/deployments/models/memory-unit.spec.ts
@@ -1,4 +1,5 @@
 import {
+  fromOrdinal,
   MemoryUnit,
   ordinal
 } from './memory-unit';
@@ -19,6 +20,32 @@ describe('MemoryUnit', () => {
 
     it('should be 3 for GB', () => {
       expect(ordinal(MemoryUnit.GB)).toBe(3);
+    });
+  });
+
+  describe('fromOrdinal', () => {
+    it('should be B for 0', () => {
+      expect(fromOrdinal(0)).toEqual(MemoryUnit.B);
+    });
+
+    it('should be KB for 1', () => {
+      expect(fromOrdinal(1)).toEqual(MemoryUnit.KB);
+    });
+
+    it('should be MB for 2', () => {
+      expect(fromOrdinal(2)).toEqual(MemoryUnit.MB);
+    });
+
+    it('should be GB for 3', () => {
+      expect(fromOrdinal(3)).toEqual(MemoryUnit.GB);
+    });
+
+    describe('undefined cases', () => {
+      [-1, Object.keys(MemoryUnit).length, 1.5, NaN, Infinity].forEach((n: number): void => {
+        it(`should be undefined for ${n}`, () => {
+          expect(fromOrdinal(n)).toBeUndefined();
+        });
+      });
     });
   });
 });
