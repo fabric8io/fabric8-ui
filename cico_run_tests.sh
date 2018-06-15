b28038f3dd0d0d54efe2e37a938aac94fecf76f3
Jaime Melis <jmelis@redhat.com>
2018-06-15 10:40:26 +0200

    Fix logic in build script

--- cico_run_tests.sh
+++ cico_run_tests.sh
@@ -24,13 +24,14 @@ set -x
 
 # We need to disable selinux for now, XXX
 /usr/sbin/setenforce 0
-echo "$(date) $line"
+
+# Print date
+date
+
 # Get all the deps in
-yum -y install \
-  docker \
-  make \
-  git
-service docker start
+yum -y install docker make git
+
+systemctl start docker
 echo "Docker Started: $(date) $line"
 
 REGISTRY="quay.io"
@@ -44,41 +45,33 @@ else
 fi
 
 mkdir -p dist
-docker run --detach=true --name=fabric8-ui-builder -t -v $(pwd)/dist:/dist:Z ${REGISTRY}/openshiftio/rhel-fabric8-ui-fabric8-ui-builder:${TAG}
 
-if [[ $? -ne 0 ]]; then
-  docker build -t fabric8-ui-builder -f Dockerfile.builder . && \
-  docker tag fabric8-ui-builder ${REGISTRY}/openshiftio/fabric8-ui-fabric8-ui-builder:${TAG} && \
+if ! docker pull ${REGISTRY}/openshiftio/rhel-fabric8-ui-fabric8-ui-builder:${TAG}; then
+  docker build -t fabric8-ui-builder -f Dockerfile.builder .
+  docker tag fabric8-ui-builder ${REGISTRY}/openshiftio/fabric8-ui-fabric8-ui-builder:${TAG}
   docker push ${REGISTRY}/openshiftio/fabric8-ui-fabric8-ui-builder:${TAG}
-  docker run --detach=true --name=fabric8-ui-builder -t -v $(pwd)/dist:/dist:Z ${REGISTRY}/openshiftio/rhel-fabric8-ui-fabric8-ui-builder:${TAG}
 fi
 
-echo "NPM Install starting: $(date) $line"
+docker run --detach=true --name=fabric8-ui-builder -t \
+  -v $(pwd)/dist:/dist:Z \
+  ${REGISTRY}/openshiftio/rhel-fabric8-ui-fabric8-ui-builder:${TAG}
+
+echo "NPM Install starting: $(date)"
 
 # Build almighty-ui
 docker exec fabric8-ui-builder npm install
-echo "NPM Install Complete: $(date) $line"
+echo "NPM Install Complete: $(date)"
+
 ## Exec unit tests
 docker exec fabric8-ui-builder ./run_unit_tests.sh
+echo 'CICO: unit tests OK'
 
-if [ $? -eq 0 ]; then
-  echo 'CICO: unit tests OK'
-  ./upload_to_codecov.sh
-else
-  echo 'CICO: unit tests FAIL'
-  exit 1
-fi
+./upload_to_codecov.sh
 
 ## Exec functional tests
 docker exec fabric8-ui-builder ./run_functional_tests.sh
+echo 'CICO: functional tests OK'
 
 ## All ok, build prod version
-if [ $? -eq 0 ]; then
-  echo 'CICO: functional tests OK'
-  docker exec fabric8-ui-builder npm run build:prod
-else
-  echo 'CICO: functional tests FAIL'
-  exit 1
-fi
+docker exec fabric8-ui-builder npm run build:prod
 echo "Build Complete: $(date) $line"
-
