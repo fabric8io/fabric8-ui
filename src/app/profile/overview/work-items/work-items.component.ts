27c14abf8cacf98520e0701887603e3d8b540ea6
Jie Kang <jkang@redhat.com>
2018-07-19 16:29:37 -0400

    fix(profile) refactor work item component code to be simpler

--- src/app/profile/overview/work-items/work-items.component.ts
+++ src/app/profile/overview/work-items/work-items.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 
+import { uniqBy } from 'lodash';
+
 import { WorkItem, WorkItemService } from 'fabric8-planner';
 import { Context, Contexts } from 'ngx-fabric8-wit';
 import { Space, Spaces, SpaceService } from 'ngx-fabric8-wit';
@@ -14,7 +16,7 @@ import { filterOutClosedItems } from '../../../shared/workitem-utils';
   templateUrl: './work-items.component.html',
   styleUrls: ['./work-items.component.less']
 })
-export class WorkItemsComponent implements OnDestroy, OnInit  {
+export class WorkItemsComponent implements OnDestroy, OnInit {
   context: Context;
   currentSpace: Space;
   currentSpaceId: string = 'default';
@@ -24,31 +26,39 @@ export class WorkItemsComponent implements OnDestroy, OnInit  {
   viewingOwnAccount: Boolean;
 
   constructor(
-      private contexts: Contexts,
-      private spacesService: Spaces,
-      private spaceService: SpaceService,
-      private workItemService: WorkItemService,
-      private contextService: ContextService) {
-  }
+    private contexts: Contexts,
+    private spacesService: Spaces,
+    private spaceService: SpaceService,
+    private workItemService: WorkItemService,
+    private contextService: ContextService
+  ) {}
 
   ngOnInit(): void {
     this.viewingOwnAccount = this.contextService.viewingOwnContext();
-    this.subscriptions.push(this.contexts.current.subscribe((ctx: Context) => {
-      this.context = ctx;
-      if (this.context.user.attributes) {
-        this.subscriptions.push(this.spaceService.getSpacesByUser(this.context.user.attributes.username).subscribe(spaces => {
-          this.spaces = spaces;
-          if (this.viewingOwnAccount) {
-            this.subscriptions.push(this.spacesService.recent.subscribe(recentSpaces => {
-              if (recentSpaces && recentSpaces.length > 0) {
-                this.mergeSpaces(this.spaces, recentSpaces);
-                this.selectSpace(recentSpaces[0]);
-              }
-            }));
-          }
-        }));
-      }
-    }));
+    this.subscriptions.push(
+      this.contexts.current.subscribe((ctx: Context) => {
+        this.context = ctx;
+        if (this.context.user.attributes) {
+          this.subscriptions.push(
+            this.spaceService
+              .getSpacesByUser(this.context.user.attributes.username)
+              .subscribe(spaces => {
+                this.spaces = spaces;
+                if (this.viewingOwnAccount) {
+                  this.subscriptions.push(
+                    this.spacesService.recent.subscribe(recentSpaces => {
+                      if (recentSpaces && recentSpaces.length > 0) {
+                        this.spaces = uniqBy(spaces.concat(recentSpaces), 'id');
+                        this.attemptSelectSpace(recentSpaces[0]);
+                      }
+                    })
+                  );
+                }
+              })
+          );
+        }
+      })
+    );
   }
 
   ngOnDestroy(): void {
@@ -57,60 +67,45 @@ export class WorkItemsComponent implements OnDestroy, OnInit  {
     });
   }
 
-  private mergeSpaces(spaces: Space[], recentSpaces: Space[]): void {
-    let found: boolean;
-    for (let i: number = 0; i < recentSpaces.length; i++) {
-      found = false;
-      for (let j: number = 0; j < spaces.length; j++) {
-        if (recentSpaces[i].id === spaces[j].id) {
-          found = true;
-          break;
-        }
-      }
-      if (!found) {
-        this.spaces.push(recentSpaces[i]);
-      }
-    }
-  }
-
-  private selectSpace(space: Space) {
-    for (let i: number = 0; i < this.spaces.length; i++) {
-      if (this.spaces[i].id === space.id) {
-        this.currentSpace = this.spaces[i];
-        this.currentSpaceId = this.currentSpace.id;
-        this.fetchWorkItemsBySpace(this.currentSpace);
-        break;
-      }
+  private attemptSelectSpace(spaceToSelect: Space) {
+    let space: Space = this.spaces.find((s: Space) => s.id == spaceToSelect.id);
+    if (space !== undefined) {
+      this.currentSpace = space;
+      this.currentSpaceId = this.currentSpace.id;
+      this.fetchWorkItemsBySpace(this.currentSpace);
     }
   }
 
   fetchWorkItems(): void {
-    this.currentSpace = this.getSpaceById(this.currentSpaceId);
-    if (this.currentSpace != null) {
+    this.currentSpace = this.spaces.find((s: Space) => s.id === this.currentSpaceId);
+    if (this.currentSpace !== undefined) {
       this.fetchWorkItemsBySpace(this.currentSpace);
     }
   }
 
   private fetchWorkItemsBySpace(space: Space): void {
-    let filters = [{
-      paramKey: 'filter[assignee]',
-      value: this.context.user.id,
-      active: true
-    }];
-    this.workItemService._currentSpace = space;
-    this.subscriptions.push(this.workItemService
-      .getWorkItems(100000, filters).subscribe(result => {
-        this.workItems = filterOutClosedItems(result.workItems);
-      })
-    );
-  }
-
-  private getSpaceById(id: string): Space {
-    for (let i: number = 0; i < this.spaces.length; i++) {
-      if (id === this.spaces[i].id) {
-        return this.spaces[i];
+    let filters = [
+      {
+        paramKey: 'filter[assignee]',
+        value: this.context.user.id,
+        active: true
       }
-    }
-    return null;
+    ];
+    this.workItemService._currentSpace = space;
+    this.subscriptions.push(
+      this.workItemService
+        .getWorkItems(100000, filters)
+        .subscribe(
+          (result: {
+            workItems: WorkItem[];
+            nextLink: string;
+            totalCount?: number;
+            included?: WorkItem[];
+            ancestorIDs?: string[];
+          }) => {
+            this.workItems = filterOutClosedItems(result.workItems);
+          }
+        )
+    );
   }
 }
