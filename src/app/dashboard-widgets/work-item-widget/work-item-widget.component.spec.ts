2f5bd54320e792e9b390118d770401e937d74d22
Alex Macdonald <almacdon@redhat.com>
2018-07-30 13:20:56 -0400

    test(work-item-widget): cleanup spec file

--- src/app/dashboard-widgets/work-item-widget/work-item-widget.component.spec.ts
+++ src/app/dashboard-widgets/work-item-widget/work-item-widget.component.spec.ts
@@ -1,81 +1,76 @@
 import { LocationStrategy } from '@angular/common';
-import { DebugNode, NO_ERRORS_SCHEMA } from '@angular/core';
+import { DebugElement, DebugNode, NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { By } from '@angular/platform-browser';
-import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { ActivatedRoute, Event, Router, RouterModule } from '@angular/router';
 
 import { WorkItem, WorkItemService } from 'fabric8-planner';
-import { Contexts } from 'ngx-fabric8-wit';
-
+import { cloneDeep } from 'lodash';
+import { Context, Contexts } from 'ngx-fabric8-wit';
 import { Feature, FeatureTogglesService } from 'ngx-feature-flag';
+import { Observable } from 'rxjs';
+
+import { createMock } from 'testing/mock';
+import { MockFeatureToggleComponent } from 'testing/mock-feature-toggle.component';
 import { LoadingWidgetModule } from '../../dashboard-widgets/loading-widget/loading-widget.module';
 import { WorkItemBarchartModule } from './work-item-barchart/work-item-barchart.module';
 import { WorkItemWidgetComponent } from './work-item-widget.component';
-
-import { cloneDeep } from 'lodash';
-import { Observable } from 'rxjs';
-
-import { MockFeatureToggleComponent } from 'testing/mock-feature-toggle.component';
 
 describe('WorkItemWidgetComponent', () => {
   let fixture: ComponentFixture<WorkItemWidgetComponent>;
   let component: DebugNode['componentInstance'];
 
-  let mockContext = {
-    'user': {
-      'attributes': {
-        'username': 'mock-username'
-      },
-      'id': 'mock-user'
-    }
-  };
-  let mockRouterEvent: any = {
+  let mockContext: Context;
+  let workItem: WorkItem;
+
+  const mockRouterEvent: Event = {
     'id': 1,
     'url': 'mock-url'
-  };
-  let workItem = {
-    attributes: {
-      description: 'description',
-      name: 'name'
-    },
-    type: 'workitems'
-  } as WorkItem;
+  } as Event;
 
-  let workItem1 = cloneDeep(workItem);
-  let workItem2 = cloneDeep(workItem);
-  let workItem3 = cloneDeep(workItem);
-  let workItem4 = cloneDeep(workItem);
-  let workItem5 = cloneDeep(workItem);
-
-  let mockActivatedRoute: any = jasmine.createSpy('ActivatedRoute');
-  let mockContexts: any = jasmine.createSpy('Contexts');
-  let mockFeatureTogglesService = jasmine.createSpyObj('FeatureTogglesService', ['getFeature']);
-  let mockLocationStrategy: any = jasmine.createSpyObj('LocationStrategy', ['prepareExternalUrl']);
-  let mockRouter = jasmine.createSpyObj('Router', ['createUrlTree', 'navigate', 'serializeUrl']);
-  let mockWorkItemService: any = jasmine.createSpyObj('WorkItemService', ['getWorkItems']);
-
-  let mockFeature: Feature = {
+  const mockFeature: Feature = {
     'attributes': {
       'name': 'mock-attribute',
       'enabled': true,
       'user-enabled': true
     }
-  };
-  mockFeatureTogglesService.getFeature.and.returnValue(Observable.of(mockFeature));
+  } as Feature;
+
+  let workItem1: WorkItem = cloneDeep(workItem);
+  let workItem2: WorkItem = cloneDeep(workItem);
+  let workItem3: WorkItem = cloneDeep(workItem);
+  let workItem4: WorkItem = cloneDeep(workItem);
+  let workItem5: WorkItem = cloneDeep(workItem);
 
   beforeEach(() => {
-    workItem1.attributes['system.state'] = 'open';
-    workItem2.attributes['system.state'] = 'open';
-    workItem3.attributes['system.state'] = 'in progress';
-    workItem4.attributes['system.state'] = 'resolved';
-    workItem5.attributes['system.state'] = 'new';
 
-    mockContexts.current = Observable.of(mockContext);
-    mockRouter.events = Observable.of(mockRouterEvent);
-    mockWorkItemService.getWorkItems.and.returnValue(Observable.of({
-      workItems: [workItem1, workItem2, workItem3, workItem4, workItem5]
-    }));
+    mockContext = {
+      'user': {
+        'attributes': {
+          'username': 'mock-username'
+        },
+        'id': 'mock-user'
+      }
+    } as Context;
+
+    workItem = {
+      attributes: {
+        description: 'description',
+        name: 'name'
+      },
+      type: 'workitems'
+    } as WorkItem;
+
+    workItem1 = cloneDeep(workItem);
+    workItem1.attributes['system.state'] = 'open';
+    workItem2 = cloneDeep(workItem);
+    workItem2.attributes['system.state'] = 'open';
+    workItem3 = cloneDeep(workItem);
+    workItem3.attributes['system.state'] = 'in progress';
+    workItem4 = cloneDeep(workItem);
+    workItem4.attributes['system.state'] = 'resolved';
+    workItem5 = cloneDeep(workItem);
+    workItem5.attributes['system.state'] = 'new';
 
     TestBed.configureTestingModule({
       imports: [
@@ -88,12 +83,54 @@ describe('WorkItemWidgetComponent', () => {
         WorkItemWidgetComponent
       ],
       providers: [
-        { provide: FeatureTogglesService, useValue: mockFeatureTogglesService },
-        { provide: ActivatedRoute, useValue: mockActivatedRoute },
-        { provide: Contexts, useFactory: () => mockContexts },
-        { provide: LocationStrategy, useValue: mockLocationStrategy },
-        { provide: Router, useValue: mockRouter },
-        { provide: WorkItemService, useValue: mockWorkItemService }
+        {
+          provide: FeatureTogglesService,
+          useFactory: () => {
+            const mockFeatureTogglesService: jasmine.SpyObj<FeatureTogglesService> = createMock(FeatureTogglesService);
+            mockFeatureTogglesService.getFeature.and.returnValue(Observable.of(mockFeature));
+            return mockFeatureTogglesService;
+          }
+        },
+        {
+          provide: ActivatedRoute,
+          useFactory: () => {
+            const mockActivatedRoute: any = jasmine.createSpy('ActivatedRoute');
+            return mockActivatedRoute;
+          }
+        },
+        {
+          provide: Contexts,
+          useFactory: () => {
+            const mockContexts: any = createMock(Contexts);
+            mockContexts.current = Observable.of(mockContext);
+            return mockContexts;
+          }
+        },
+        {
+          provide: LocationStrategy,
+          useFactory: () => {
+            const mockLocationStrategy: jasmine.SpyObj<LocationStrategy> = jasmine.createSpyObj('LocationStrategy', ['prepareExternalUrl']);
+            return mockLocationStrategy;
+          }
+        },
+        {
+          provide: Router,
+          useFactory: () => {
+            const mockRouter: any = jasmine.createSpyObj('Router', ['createUrlTree', 'navigate', 'serializeUrl']);
+            mockRouter.events = Observable.of(mockRouterEvent);
+            return mockRouter;
+          }
+        },
+        {
+          provide: WorkItemService,
+          useFactory: () => {
+            const mockWorkItemService: jasmine.SpyObj<WorkItemService> = createMock(WorkItemService);
+            mockWorkItemService.getWorkItems.and.returnValue(Observable.of({
+              workItems: [workItem1, workItem2, workItem3, workItem4, workItem5]
+            }));
+            return mockWorkItemService;
+          }
+        }
       ],
       schemas: [NO_ERRORS_SCHEMA]
     });
@@ -103,27 +140,27 @@ describe('WorkItemWidgetComponent', () => {
   });
 
   it('Should output open count', () => {
-    let element = fixture.debugElement.query(By.css('#open'));
+    let element: DebugElement = fixture.debugElement.query(By.css('#open'));
     expect(element.nativeElement.textContent.trim().slice(0, '2'.length)).toBe('2');
   });
 
   it('Should output resolved count', () => {
-    let element = fixture.debugElement.query(By.css('#resolved'));
+    let element: DebugElement = fixture.debugElement.query(By.css('#resolved'));
     expect(element.nativeElement.textContent.trim().slice(0, '2'.length)).toBe('1');
   });
 
   it('Should output in progress count', () => {
-    let element = fixture.debugElement.query(By.css('#in-progress'));
+    let element: DebugElement = fixture.debugElement.query(By.css('#in-progress'));
     expect(element.nativeElement.textContent.trim().slice(0, '1'.length)).toBe('1');
   });
 
   it('Should output total count', () => {
-    let element = fixture.debugElement.query(By.css('#total'));
+    let element: DebugElement = fixture.debugElement.query(By.css('#total'));
     expect(element.nativeElement.textContent.trim().slice(0, '5'.length)).toBe('5');
   });
 
   it('Should output a bar chart', () => {
-    let elements = fixture.debugElement.queryAll(By.css('fabric8-work-item-barchart div'));
+    let elements: DebugElement[] = fixture.debugElement.queryAll(By.css('fabric8-work-item-barchart div'));
     expect(elements.length).toBe(1);
   });
 
@@ -142,4 +179,5 @@ describe('WorkItemWidgetComponent', () => {
 
     expect(fixture.debugElement.query(By.css('#spacehome-workitems-create-button'))).toBeNull();
   });
+
 });
