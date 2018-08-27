5cad5630767e97925e9d4b166b671000b5effd4e
Rohit Rai <rohitkrai03@gmail.com>
2018-08-28 02:26:08 +0530

    feat(interceptors): add interceptor for X-Request-Id header

--- src/app/shared/interceptors/index.ts
+++ src/app/shared/interceptors/index.ts
@@ -2,8 +2,10 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { CacheInterceptor } from './cache.interceptor';
+import { RequestIdInterceptor } from './request-id.interceptor';
 
 /** Http interceptor providers in outside-in order */
 export const HttpInterceptorProviders = [
+  { provide: HTTP_INTERCEPTORS, useClass: RequestIdInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: CacheInterceptor, multi: true }
 ];
