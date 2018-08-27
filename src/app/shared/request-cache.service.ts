ef5e9f5cdaa0f8c1efbd05ae8105928ed5aaf53c
Rohit Rai <rohitkrai03@gmail.com>
2018-08-27 19:52:27 +0530

    feat(interceptors): add caching interceptor

---
+++ src/app/shared/request-cache.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+
+import { HttpRequest, HttpResponse } from '@angular/common/http';
+
+
+export interface RequestCacheEntry {
+  url: string;
+  response: HttpResponse<any>;
+  lastRead: number;
+}
+
+export abstract class RequestCache {
+  abstract get(req: HttpRequest<any>): HttpResponse<any> | undefined;
+  abstract set(req: HttpRequest<any>, response: HttpResponse<any>): void;
+}
+
+const maxAge = 300000; // maximum cache age (ms)
+
+@Injectable()
+export class RequestCacheWithMap implements RequestCache {
+
+  cache = new Map<string, RequestCacheEntry>();
+
+  get(req: HttpRequest<any>): HttpResponse<any> | undefined {
+    const url = req.urlWithParams;
+    const cached = this.cache.get(url);
+    console.log(`Found cached response for ${url}`);
+
+    if (!cached) {
+      return undefined;
+    }
+
+    const isExpired = cached.lastRead < (Date.now() - maxAge);
+    const expired = isExpired ? 'expired ' : '';
+    console.log(`Found ${expired}cached response for "${url}".`);
+    return isExpired ? undefined : cached.response;
+  }
+
+  set(req: HttpRequest<any>, response: HttpResponse<any>): void {
+    const url = req.urlWithParams;
+    console.log(`Caching response from "${url}".`);
+
+    const entry = { url, response, lastRead: Date.now() };
+    this.cache.set(url, entry);
+
+    // remove expired cache entries
+    const expired = Date.now() - maxAge;
+    this.cache.forEach(entry => {
+      if (entry.lastRead < expired) {
+        this.cache.delete(entry.url);
+      }
+    });
+
+    console.log(`Request cache size: ${this.cache.size}.`);
+  }
+}
