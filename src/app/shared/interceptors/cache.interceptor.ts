8f70cc2be86f0c889a5ba7536e5fd933803183b6
Rohit Rai <rohitkrai03@gmail.com>
2018-08-28 01:45:57 +0530

    feat(interceptors): logic for cache request changed using AsyncSubject

--- src/app/shared/interceptors/cache.interceptor.ts
+++ src/app/shared/interceptors/cache.interceptor.ts
@@ -1,22 +1,16 @@
-// #docplaster
 import { Injectable } from '@angular/core';
 
 import {
-  HttpEvent, HttpHandler,
-  HttpInterceptor, HttpRequest, HttpResponse
+  HttpHandler, HttpInterceptor,
+  HttpRequest, HttpResponse
 } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
-import { startWith, tap } from 'rxjs/operators';
-
+import { AsyncSubject, Observable, Scheduler } from 'rxjs';
 import { RequestCache } from '../request-cache.service';
 
 /**
- * If request is cachable (e.g., package search) and
+ * If request is cachable (e.g., GET) and
  * response is in cache return the cached response as observable.
- * If has 'x-refresh' header that is true,
- * then also re-run the package search, using response from next(),
- * returning an observable that emits the cached response first.
  *
  * If not in cache or not cachable,
  * pass request through to next()
@@ -30,16 +24,16 @@ export class CacheInterceptor implements HttpInterceptor {
     if (!isCachable(req)) { return next.handle(req); }
 
     const cachedResponse = this.cache.get(req);
-    // cache-then-refresh
-    if (req.headers.get('x-refresh')) {
-      const results$ = sendRequest(req, next, this.cache);
-      return cachedResponse ?
-        results$.pipe(startWith(cachedResponse)) :
-        results$;
-    }
-    // cache-or-fetch
-    return cachedResponse ?
-      Observable.of(cachedResponse) : sendRequest(req, next, this.cache);
+
+    return Observable.create(observer => {
+      if (cachedResponse) {
+        return cachedResponse.subscribeOn(Scheduler.async).subscribe(observer);
+      } else {
+        const asyncResponse = new AsyncSubject<HttpResponse<any>>();
+        this.cache.set(req, asyncResponse);
+        return next.handle(req).subscribe(asyncResponse);
+      }
+    });
   }
 }
 
@@ -49,23 +43,3 @@ function isCachable(req: HttpRequest<any>) {
   // Only GET requests are cachable
   return req.method === 'GET';
 }
-
-/**
- * Get server response observable by sending request to `next()`.
- * Will add the response to the cache on the way out.
- */
-function sendRequest(
-  req: HttpRequest<any>,
-  next: HttpHandler,
-  cache: RequestCache): Observable<HttpEvent<any>> {
-
-  return next.handle(req).pipe(
-    tap(event => {
-      // There may be other events besides the response.
-      if (event instanceof HttpResponse) {
-        cache.set(req, event); // Update the cache.
-      }
-    })
-  );
-}
-
--- src/app/shared/request-cache.service.ts
+++ src/app/shared/request-cache.service.ts
@@ -1,51 +1,60 @@
 import { Injectable } from '@angular/core';
 
 import { HttpRequest, HttpResponse } from '@angular/common/http';
+import { AsyncSubject } from 'rxjs';
 
 
-export interface RequestCacheEntry {
-  url: string;
-  response: HttpResponse<any>;
+export interface RequestCacheItem {
+  asyncResponse: AsyncSubject<HttpResponse<any>>;
   lastRead: number;
 }
 
-const maxAge = 300; // maximum cache age (ms)
+const CACHE_TTL = 300; // maximum cache age (ms)
 
 @Injectable()
 export class RequestCache {
 
-  cache = new Map<string, RequestCacheEntry>();
+  cache = new Map<string, RequestCacheItem>();
 
-  get(req: HttpRequest<any>): HttpResponse<any> | undefined {
-    const url = req.urlWithParams;
-    const cached = this.cache.get(url);
-    console.log(`Found cached response for ${url}`);
+  get(req: HttpRequest<any>): AsyncSubject<HttpResponse<any>> | undefined {
+    const cacheKey = createCacheKey(req);
+    const cached = this.cache.get(cacheKey);
 
     if (!cached) {
       return undefined;
     }
 
-    const isExpired = cached.lastRead < (Date.now() - maxAge);
+    const isExpired = cached.lastRead < (Date.now() - CACHE_TTL);
     const expired = isExpired ? 'expired ' : '';
-    console.log(`Found ${expired}cached response for "${url}".`);
-    return isExpired ? undefined : cached.response;
+    console.log(`Found ${expired}cached response for "${req.url}".`);
+    return isExpired ? undefined : cached.asyncResponse;
   }
 
-  set(req: HttpRequest<any>, response: HttpResponse<any>): void {
-    const url = req.urlWithParams;
-    console.log(`Caching response from "${url}".`);
+  set(req: HttpRequest<any>, asyncResponse: AsyncSubject<HttpResponse<any>>): void {
+    const cacheKey = createCacheKey(req);
+    console.log(`Caching response from "${req.url}".`);
 
-    const entry = { url, response, lastRead: Date.now() };
-    this.cache.set(url, entry);
+    const cacheItem = { asyncResponse, lastRead: Date.now() };
+    this.cache.set(cacheKey, cacheItem);
 
     // remove expired cache entries
-    const expired = Date.now() - maxAge;
-    this.cache.forEach(entry => {
-      if (entry.lastRead < expired) {
-        this.cache.delete(entry.url);
+    const expired = Date.now() - CACHE_TTL;
+    this.cache.forEach(cacheItem => {
+      if (cacheItem.lastRead < expired) {
+        this.cache.delete(cacheKey);
       }
     });
 
     console.log(`Request cache size: ${this.cache.size}.`);
   }
+}
+
+// Creates a cache key from headers, query params, and URL
+function createCacheKey(req: HttpRequest<any>): string {
+  let key;
+  key = req.urlWithParams;
+  if (req.headers) {
+    key += `:${JSON.stringify(req.headers)}`;
+  }
+  return key;
 }
