48f4eb283f9738b7d445f37a9d5a3e8789478961
Andrew Azores <me@andrewazor.es>
2018-09-06 15:09:11 -0400

    feat(add-codebase-widget): replace placeholder text
    Replace placeholder "last updated" text with actual Git "pushed_at"
    timestamp
    
    fixes https://openshift.io/openshiftio/Openshift_io/plan/detail/542

--- src/app/dashboard-widgets/add-codebase-widget/codebase-item/codebase-item.component.ts
+++ src/app/dashboard-widgets/add-codebase-widget/codebase-item/codebase-item.component.ts
@@ -1,15 +1,37 @@
 import {
   Component,
-  Input
+  Input,
+  OnInit
 } from '@angular/core';
 
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators/map';
+
 import { Codebase } from '../../../space/create/codebases/services/codebase';
+import { GitHubRepoDetails } from '../../../space/create/codebases/services/github';
+import { GitHubService } from '../../../space/create/codebases/services/github.service';
 
 @Component({
   selector: 'fabric8-add-codebase-widget-codebase-item',
   templateUrl: './codebase-item.component.html',
-  styleUrls: ['./codebase-item.component.less']
+  styleUrls: ['./codebase-item.component.less'],
+  providers: [GitHubService]
 })
-export class CodebaseItemComponent {
+export class CodebaseItemComponent implements OnInit {
+
   @Input() codebase: Codebase;
+  lastUpdated$: Observable<string>;
+
+  constructor(
+    private readonly githubService: GitHubService
+  ) { }
+
+  ngOnInit(): void {
+    this.lastUpdated$ = this.githubService
+      .getRepoDetailsByUrl(this.codebase.attributes.url)
+      .pipe(
+        map((details: GitHubRepoDetails): string => details.pushed_at)
+      );
+  }
+
 }
