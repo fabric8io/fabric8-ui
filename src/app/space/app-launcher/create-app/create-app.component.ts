45dcdbe8ddc1af9cf6cc5d77d2b056b8cb3f962f
Vikram Raj <vikramraj1991@gmail.com>
2018-09-11 16:49:57 +0530

    fix(launcher): modify feature flag for f8launcher component

--- src/app/space/app-launcher/create-app/create-app.component.ts
+++ src/app/space/app-launcher/create-app/create-app.component.ts
@@ -5,8 +5,11 @@ import {
   ViewEncapsulation
 } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Rx';
+
 import { Broadcaster } from 'ngx-base';
 import { Context, Space } from 'ngx-fabric8-wit';
+import { FeatureTogglesService } from 'ngx-feature-flag';
 import { User, UserService } from 'ngx-login-client';
 import { Subscription } from 'rxjs';
 import { ContextService } from '../../../shared/context.service';
@@ -21,11 +24,14 @@ export class CreateAppComponent implements OnDestroy, OnInit {
   loggedInUser: User;
   spaces: Space[] = [];
   subscriptions: Subscription[] = [];
+  depEditorEnable: Observable<{} | boolean>;
+  nextButtonsEnable: Observable<{} | boolean>;
 
   constructor(private context: ContextService,
               private userService: UserService,
               private router: Router,
-              private broadcaster: Broadcaster) {
+              private broadcaster: Broadcaster,
+              private  featureToggleService: FeatureTogglesService) {
     this.subscriptions.push(userService.loggedInUser.subscribe(user => {
       this.loggedInUser = user;
     }));
@@ -44,6 +50,8 @@ export class CreateAppComponent implements OnDestroy, OnInit {
     this.broadcaster.broadcast('analyticsTracker', {
       event: 'create app opened'
     });
+    this.depEditorEnable = this.featureToggleService.isFeatureUserEnabled('AppLauncher.DependencyEditor');
+    this.nextButtonsEnable = this.featureToggleService.isFeatureUserEnabled('AppLauncher.NextButtons');
   }
 
   /**
--- src/app/space/app-launcher/import-app/import-app.component.ts
+++ src/app/space/app-launcher/import-app/import-app.component.ts
@@ -5,8 +5,11 @@ import {
   ViewEncapsulation
 } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Rx';
+
 import { Broadcaster } from 'ngx-base';
 import { Context, Space } from 'ngx-fabric8-wit';
+import { FeatureTogglesService } from 'ngx-feature-flag';
 import { User, UserService } from 'ngx-login-client';
 import { Subscription } from 'rxjs';
 import { ContextService } from '../../../shared/context.service';
@@ -21,11 +24,13 @@ export class ImportAppComponent implements OnDestroy, OnInit {
   loggedInUser: User;
   spaces: Space[] = [];
   subscriptions: Subscription[] = [];
+  nextButtonsEnable: Observable<{} | boolean>;
 
   constructor(private context: ContextService,
               private userService: UserService,
               private router: Router,
-              private broadcaster: Broadcaster) {
+              private broadcaster: Broadcaster,
+              private  featureToggleService: FeatureTogglesService) {
     this.subscriptions.push(userService.loggedInUser.subscribe(user => {
       this.loggedInUser = user;
     }));
@@ -44,6 +49,7 @@ export class ImportAppComponent implements OnDestroy, OnInit {
     this.broadcaster.broadcast('analyticsTracker', {
       event: 'import app opened'
     });
+    this.nextButtonsEnable = this.featureToggleService.isFeatureUserEnabled('AppLauncher.NextButtons');
   }
 
   /**
