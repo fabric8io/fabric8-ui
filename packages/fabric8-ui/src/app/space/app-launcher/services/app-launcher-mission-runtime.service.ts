88f728ceb2bb877e137d90aa2e910f15932db4ca
Vikram Raj <vikrmrj@gmail.com>
2018-12-17 11:39:35 +0530

    fix(mission-runtime.service): add FeatureToggleService to providers

--- packages/fabric8-ui/src/app/space/app-launcher/services/app-launcher-mission-runtime.service.ts
+++ packages/fabric8-ui/src/app/space/app-launcher/services/app-launcher-mission-runtime.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Catalog, HelperService, MissionRuntimeService } from 'ngx-launcher';
 import { AuthenticationService } from 'ngx-login-client';
 import { Observable, throwError as observableThrowError } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { catchError, map, switchMap } from 'rxjs/operators';
 import { FeatureTogglesService } from 'ngx-feature-flag';
 
 @Injectable()
@@ -12,7 +12,6 @@ export class AppLauncherMissionRuntimeService extends MissionRuntimeService {
   private END_POINT: string = '';
   private API_BASE: string = 'booster-catalog/';
   private ORIGIN: string = '';
-  private isNodeBoosterEnable: boolean | {};
 
   constructor(
     private http: HttpClient,
@@ -26,25 +25,27 @@ export class AppLauncherMissionRuntimeService extends MissionRuntimeService {
     if (this.auth.getToken() != null) {
       this.headers = this.headers.set('Authorization', `Bearer ${this.auth.getToken()}`);
     }
-    this.featureToggleService
-      .isFeatureUserEnabled('AppLauncher.NodeBooster')
-      .subscribe((feature: boolean) => {
-        this.isNodeBoosterEnable = feature;
-      });
   }
 
   getCatalog(): Observable<Catalog> {
     if (this.ORIGIN) {
       this.headers = this.headers.set('X-App', this.ORIGIN);
     }
-    return this.http.get<Catalog>(this.END_POINT + this.API_BASE, { headers: this.headers }).pipe(
-      map((catalog: Catalog) => {
-        if (!this.isNodeBoosterEnable) {
-          catalog.boosters = catalog.boosters.filter((b) => b.runtime !== 'nodejs');
-          catalog.runtimes = catalog.runtimes.filter((r) => r.id !== 'nodejs');
-          return catalog;
-        }
-        return catalog;
+    return this.featureToggleService.isFeatureUserEnabled('AppLauncher.NodeBooster').pipe(
+      switchMap((flag: boolean) => {
+        return this.http
+          .get<Catalog>(this.END_POINT + this.API_BASE, { headers: this.headers })
+          .pipe(
+            map((catalog: Catalog) => {
+              if (!flag) {
+                catalog.boosters = catalog.boosters.filter((b) => b.runtime !== 'nodejs');
+                catalog.runtimes = catalog.runtimes.filter((r) => r.id !== 'nodejs');
+                return catalog;
+              }
+              return catalog;
+            }),
+            catchError(this.handleError),
+          );
       }),
       catchError(this.handleError),
     );
