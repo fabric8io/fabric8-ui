b55186e952ae77f2196a6d2715efc88337c9d209
karthik <karthik.jk11@gmail.com>
2019-01-08 12:34:34 +0530

    fix(login_process): change home components as a wrapper component

--- packages/fabric8-ui/src/app/home/home-dashboard/home-dashboard.component.ts
+++ packages/fabric8-ui/src/app/home/home-dashboard/home-dashboard.component.ts
@@ -1,33 +1,24 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { User, UserService, AuthenticationService } from 'ngx-login-client';
+import { User, UserService } from 'ngx-login-client';
 import { first } from 'rxjs/operators';
-import { UserSpacesService } from '../shared/user-spaces.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ProviderService } from '../shared/account/provider.service';
+import { UserSpacesService } from '../../shared/user-spaces.service';
 import { Subscription } from 'rxjs';
 
 @Component({
   encapsulation: ViewEncapsulation.None,
-  selector: 'alm-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.less'],
+  selector: 'alm-home-dashboard',
+  templateUrl: './home-dashboard.component.html',
+  styleUrls: ['./home-dashboard.component.less'],
   providers: [UserSpacesService],
 })
-export class HomeComponent implements OnInit {
+export class HomeDashboardComponent implements OnInit {
   loggedInUser: User;
   spacesCount: number = -1;
-  openShiftLinked: boolean = false;
   subscriptions: Subscription[] = [];
-  errorConnecting: boolean = false;
-  loading: boolean = true;
 
   constructor(
     private readonly userService: UserService,
     private readonly userSpacesService: UserSpacesService,
-    private readonly auth: AuthenticationService,
-    private readonly route: ActivatedRoute,
-    private readonly providerService: ProviderService,
-    private readonly router: Router,
   ) {}
 
   ngOnInit() {
@@ -38,40 +29,9 @@ export class HomeComponent implements OnInit {
       .subscribe(
         (spacesCount: number): void => {
           this.spacesCount = spacesCount;
-          if (this.loggedInUser.attributes && this.loggedInUser.attributes.cluster) {
-            this.linkOpenshiftAccounts();
-          }
         },
       );
     this.subscriptions.push(userSpaceCount);
-  }
-
-  linkOpenshiftAccounts() {
-    this.loading = true;
-    let connectionSub = this.auth
-      .isOpenShiftConnected(this.loggedInUser.attributes.cluster)
-      .subscribe((isConnected) => {
-        this.openShiftLinked = isConnected;
-        console.log('connection', this.openShiftLinked);
-        let wait = this.route.snapshot.queryParams['wait'];
-        if (!isConnected && !wait) {
-          // first time through and user isn't connected - automatically connect accounts
-          this.providerService.linkOpenShift(
-            this.loggedInUser.attributes.cluster,
-            window.location.origin + '/_home?wait=true',
-          );
-        } else if (!isConnected && wait) {
-          // second time through - do not try again if wait is on URL and the user is still not connected
-          // something happened, show error
-          this.errorConnecting = true;
-          this.loading = false;
-        } else {
-          // success - user is connected - send home
-          this.loading = false;
-          this.router.navigateByUrl('_home', { queryParams: {} });
-        }
-      });
-    this.subscriptions.push(connectionSub);
   }
 
   ngOnDestroy(): void {
--- packages/fabric8-ui/src/app/home/home-dashboard/home-dashboard.module.ts
+++ packages/fabric8-ui/src/app/home/home-dashboard/home-dashboard.module.ts
@@ -5,14 +5,14 @@ import { ModalModule } from 'ngx-bootstrap/modal';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { Fabric8WitModule } from 'ngx-fabric8-wit';
 import { FeatureFlagModule } from 'ngx-feature-flag';
-import { LoadingWidgetModule } from '../dashboard-widgets/loading-widget/loading-widget.module';
-import { RecentPipelinesWidgetModule } from '../dashboard-widgets/recent-pipelines-widget/recent-pipelines-widget.module';
-import { RecentWorkspacesWidgetModule } from '../dashboard-widgets/recent-workspaces-widget/recent-workspaces-widget.module';
-import { EmptyStateModule } from './empty-state/empty-state.module';
-import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home.component';
-import { RecentSpacesWidgetModule } from './recent-spaces-widget/recent-spaces-widget.module';
-import { WorkItemWidgetModule } from './work-item-widget/work-item-widget.module';
+import { LoadingWidgetModule } from '../../dashboard-widgets/loading-widget/loading-widget.module';
+import { RecentPipelinesWidgetModule } from '../../dashboard-widgets/recent-pipelines-widget/recent-pipelines-widget.module';
+import { RecentWorkspacesWidgetModule } from '../../dashboard-widgets/recent-workspaces-widget/recent-workspaces-widget.module';
+import { EmptyStateModule } from '.././empty-state/empty-state.module';
+import { HomeRoutingModule } from '../home-routing.module';
+import { HomeDashboardComponent } from './home-dashboard.component';
+import { RecentSpacesWidgetModule } from '../recent-spaces-widget/recent-spaces-widget.module';
+import { WorkItemWidgetModule } from '../work-item-widget/work-item-widget.module';
 
 @NgModule({
   imports: [
@@ -30,8 +30,9 @@ import { WorkItemWidgetModule } from './work-item-widget/work-item-widget.module
     ModalModule.forRoot(),
     TooltipModule.forRoot(),
   ],
-  declarations: [HomeComponent],
+  exports: [HomeDashboardComponent],
+  declarations: [HomeDashboardComponent],
 })
-export class HomeModule {
+export class HomeDashboardModule {
   constructor() {}
 }
